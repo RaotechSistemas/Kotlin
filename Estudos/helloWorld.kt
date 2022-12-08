974a98dc9f8337403b5273d9cedc9d785f2ec4da
Reginaldo Araujo Oliveira <rao.corretor@gmail.com>
2022-12-08 08:33:11 -0300

    Exercicios em Kotlin
    Hello World!

---
+++ Estudos/helloWorld.kt
@@ -0,0 +1,4 @@
+package org.kotlinlang.play // Definição do pacote (algo muito comum para organização de código
+fun main() { // Ponto de entrada da função Kotlin, definido pela função main()
+println("Hello, World!") // imprime a saída de texto, pulando uma linha
+}
